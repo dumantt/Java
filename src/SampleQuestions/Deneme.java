2d5ca981b2a9292ea099e9aa1321a36cd03b349c
dumantt <120819216+dumantt@users.noreply.github.com>
2023-03-20 23:23:25 +0100

    if parantezsiz deneme

--- src/SampleQuestions/Deneme.java
+++ src/SampleQuestions/Deneme.java
@@ -1,8 +1,18 @@
 package SampleQuestions;
 
-public class Deneme {
-    public static void main(String[] args) {
+public class Deneme  {
+            String name = "Alex";
 
+            public static void main(String[] args) {
+                Deneme MyObject = new Deneme();
+                MyObject.name = "Carlos" ;
+                System.out.println(MyObject.name);
+                Deneme2(MyObject.name);
+                System.out.println(MyObject.name);
+                }
 
+    public static void Deneme2(String name) {
+                name = "mertens";
+        System.out.println(name);
     }
 }
