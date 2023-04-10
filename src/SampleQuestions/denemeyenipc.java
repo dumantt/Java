087781c794220529fd3dba3b853708c10306af15
dumantt <120819216+dumantt@users.noreply.github.com>
2023-04-10 23:58:04 +0200

    doldurma amacli

--- src/SampleQuestions/denemeyenipc.java
+++ src/SampleQuestions/denemeyenipc.java
@@ -4,5 +4,7 @@ public class denemeyenipc {
 
     public static void main(String[] args) {
         System.out.println("Deneme 1 2");
+
+        System.out.println("doldurma icin yapiliyor =)");
     }
 }
