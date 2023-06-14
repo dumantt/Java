f8cdf604fbfe4202deb83f78c64f5fe6b3336556
dumantt <120819216+dumantt@users.noreply.github.com>
2023-06-14 20:31:10 +0200

    14/06/23 second

--- src/day01_programlamayaGiris/C02Variables.java
+++ src/day01_programlamayaGiris/C02Variables.java
@@ -1,14 +1,14 @@
 package day01_programlamayaGiris;
 
 public class C02Variables {
-    public static void main(String[] args) {int sayi=20;
+    public static void main(String[] args) {
+        int sayi = 20;
         System.out.println(sayi);
         // " işaretinin içindekini aynen yazdırır işaret yoksa bunu variable olarak kabul eder
-       sayi=30;
+        sayi = 30;
         System.out.println(sayi);
-        sayi=sayi+20;
+        sayi = sayi + 20;
         System.out.println(sayi);
-
 
     }
 }
