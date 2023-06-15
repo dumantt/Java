0655b9d4356e27e8d4111fff8f5f0cc4c72bf5e2
dumantt <120819216+dumantt@users.noreply.github.com>
2023-06-15 17:52:47 +0200

    15/06/23 third

--- src/day02_variables_scanner/C10_Sacnner.java
+++ src/day02_variables_scanner/C10_Sacnner.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class C10_Sacnner {
     public static void main(String[] args) {
-        // kullanicidan bir double bir de int sayi alip bunlarin toplamini vr csrpimini yazdirin
+        // kullanicidan bir double bir de int sayi alip bunlarin toplamini ve carpimini yazdirin
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Lütfen bir ondalık sayı girin");
+        System.out.println("Lütfen bir ondalık sayı giriniz");
         double sayiDbl = scan.nextDouble();
-        System.out.println("Lütfen bir tam sayı girin");
+        System.out.println("Lütfen bir tam sayı giriniz");
         int sayiInt = scan.nextInt();
         System.out.println("Sayilarin toplami : " + (sayiInt+sayiDbl));
         System.out.println("Sayilarin carpimi : " + (sayiInt*sayiDbl));
